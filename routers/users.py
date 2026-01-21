9db2061e8075d10b0c9c1fab264cecbc164fc425
unknown <Dominik Rmhp>
2026-01-21 13:15:09 +0100

    Added register endpoint

---
+++ routers/users.py
@@ -0,0 +1,40 @@
+import logging
+from fastapi import APIRouter, HTTPException
+from models.user import User, UserRequest, UserResponse
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from database import get_db
+from utils import hash_password 
+
+router = APIRouter()
+
+@router.post("/register", response_model=UserResponse)
+def create_user(user_req: UserRequest, db: Session = Depends(get_db)):
+    logging.info("Endpoint called.")
+
+    # Check if username exists
+    existing_user = db.query(User).filter(User.username == user_req.username).first()
+    if existing_user:
+        raise HTTPException(status_code = 409, detail = f"Username {user_req.username} already taken.")
+    
+    # Check if email is already registered
+    existing_email = db.query(User).filter(User.email == user_req.email).first()
+    if existing_email:
+        raise HTTPException(status_code = 409, detail = f"Email {user_req.email} is already registered.")
+    
+    # Create new user
+    new_user = User(
+        username = user_req.username,
+        fullname = user_req.fullname,
+        email = user_req.email,
+        hashed_password = hash_password(user_req.password)
+    )
+    logging.info(f"Created user: {new_user}.")
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    
+    response = UserResponse(username = new_user.username, email = new_user.email)
+
+    return response
